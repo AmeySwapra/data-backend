6fc95f3acebc32955884ca54e101c8a35e309e2c
AmeySwapra <ameyswapratechnologies@gmail.com>
2024-10-09 17:02:40 +0530

    origins updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: ['http://localhost:5173', 'https://data-visualziation-dashboard.netlify.app'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 }));
 app.use(express.json());
