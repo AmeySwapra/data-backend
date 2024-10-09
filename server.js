e093af2789ec4a7b4ebd897c8333bfd39d9d50f3
AmeySwapra <ameyswapratechnologies@gmail.com>
2024-10-09 16:15:06 +0530

    connection string ip address

--- server.js
+++ server.js
@@ -13,11 +13,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
